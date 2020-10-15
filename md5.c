fa89fa6172d2a137c0e4c3f44717d974adde3f90
Pai Peng <p.peng@s2icode.com>
2020-10-15 18:06:36 +0800

    remvoe main function

--- md5.c
+++ md5.c
@@ -3,10 +3,10 @@
  *
  * Compile with: gcc -o md5 md5.c
  */
+#include "md5.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdint.h>
  
 // Constants are the integer part of the sines of integers (in radians) * 2^32.
 const uint32_t k[64] = {
@@ -144,31 +144,4 @@ void md5(const uint8_t *initial_msg, size_t initial_len, uint8_t *digest) {
     to_bytes(h1, digest + 4);
     to_bytes(h2, digest + 8);
     to_bytes(h3, digest + 12);
-}
- 
-int main(int argc, char **argv) {
-    char *msg;
-    size_t len;
-    int i;
-    uint8_t result[16];
- 
-    if (argc < 2) {
-        printf("usage: %s 'string'\n", argv[0]);
-        return 1;
-    }
-    msg = argv[1];
- 
-    len = strlen(msg);
- 
-    // benchmark
-    for (i = 0; i < 1000000; i++) {
-        md5((uint8_t*)msg, len, result);
-    }
- 
-    // display result
-    for (i = 0; i < 16; i++)
-        printf("%2.2x", result[i]);
-    puts("");
- 
-    return 0;
 }
